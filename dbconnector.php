73cc53eb4f9c96669d94a772f2b7150296d73161
Corey Mundy <corey.mundy@alpinedatasolutions.com>
2022-08-05 09:08:12 -0600

    Made the connector Class
    Made the connector Class

--- dbconnector.php
+++ dbconnector.php
@@ -1,41 +1,28 @@
 <?php
-    require "MyHeader.php"
+
+// Create constants
+DEFINE ('DB_USER', 'phpa');
+DEFINE ('DB_PSWD', 'test');
+DEFINE ('DB_SERVER', 'localhost:3306');
+DEFINE ('DB_NAME', 'MobaHolder');
+
+function ConnGet() {
+    // $dbConn will contain a resource link to the database
+    // @ Don't display error
+    $dbConn = @mysqli_connect(DB_SERVER, DB_USER, DB_PSWD, DB_NAME, 3308)
+    OR die('Failed to connect to MySQL ' . DB_SERVER . '::' . DB_NAME . ' : ' . mysqli_connect_error()); // Display messge and end PHP script
+
+    return $dbConn;
+}
+
+function MyJoinGet($dbConn) {
+
+    //Query db for all information
+    $query = "SELECT * FROM `Characters` WHERE 1";
+
+    return @mysqli_query($dbConn, $query);
+}
+
 ?>
 
-<div>
 
-    <?php
-    include_once "dbConnector.php";
-
-    // Get the db connection
-    $myDbConn = ConnGet();
-    $dataSet = MyJoinGet($myDbConn);
-
-    if ($dataSet){
-        echo '<table align="center" cellspacing="5" cellpadding="8">
-        <tr><td align="center"><b>God Name</b></td>
-        <td align="center"><b>Class</b></td>';
-        while($row = mysqli_fetch_array($dataSet)){
-            echo '<tr><td align="center">' .
-            $row['Name'] . '</td><td align="center">' .
-            $row['Class'];
-            echo '</tr>';
-        }
-        mysqli_free_result($dataSet);
-    } // End if
-    else {
-        echo "Query failed<br />";
-        echo mysqli_error($myDbConn);
-    }
-
-    echo "</Table> <br /> <br/>";
-
-    // Always close the connection
-    mysqli_close($myDbConn);
-    ?>
-
-</div>
-
-<?php
-include_once "MyFooter.php";
-?>
